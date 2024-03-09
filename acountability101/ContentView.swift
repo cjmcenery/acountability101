6fbf0c5dcfdafa17eb5c096c0845bee87a479cb0
Connor McEnery <61913573+cjmcenery@users.noreply.github.com>
2024-03-09 17:21:09 -0600

    Initial Commit

---
+++ acountability101/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  acountability101
+//
+//  Created by Connor McEnery on 3/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ acountability101/acountability101App.swift
@@ -0,0 +1,17 @@
+//
+//  acountability101App.swift
+//  acountability101
+//
+//  Created by Connor McEnery on 3/9/24.
+//
+
+import SwiftUI
+
+@main
+struct acountability101App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
