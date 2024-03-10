0d1b8255822359b3b36d8666709b62672b5b8379
Connor McEnery <61913573+cjmcenery@users.noreply.github.com>
2024-03-10 09:54:51 -0500

    Initital Commit

--- acountability101/views/GroupCard.swift
+++ acountability101/views/GroupCard.swift
@@ -8,11 +8,65 @@
 import SwiftUI
 
 struct GroupCard: View {
+    @State var groupName: String
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text(groupName).font(.system(size: 20, weight: .bold))
+            ScrollView(.horizontal) {
+                LazyHStack{
+                    VStack {
+                        HStack(spacing: 10) {
+                            RoundedRectangle(cornerRadius: 10).frame(width: 160, height: 190)
+                            VStack {
+                                Circle().frame(width: 75, height: 75)
+                                Text("5 min ago").font(.system(size: 10))
+                                Circle().frame(width: 75, height: 75)
+                                Text("5 min ago").font(.system(size: 10))
+                            }
+                            VStack {
+                                Circle().frame(width: 75, height: 75)
+                                Text("5 min ago").font(.system(size: 10))
+                                Circle().frame(width: 75, height: 75)
+                                Text("5 min ago").font(.system(size: 10))
+                            }
+                        }
+                    }
+                    VStack {
+                        HStack(spacing: 10) {
+                            VStack {
+                                Circle().frame(width: 75, height: 75)
+                                Text("5 min ago").font(.system(size: 10))
+                                Circle().frame(width: 75, height: 75)
+                                Text("5 min ago").font(.system(size: 11))
+                            }
+                            VStack {
+                                Circle().frame(width: 75, height: 75)
+                                Text("5 min ago").font(.system(size: 10))
+                                Circle().frame(width: 75, height: 75)
+                                Text("5 min ago").font(.system(size: 10))
+                            }
+                            VStack {
+                                Circle().frame(width: 75, height: 75)
+                                Text("5 min ago").font(.system(size: 10))
+                                Circle().frame(width: 75, height: 75)
+                                Text("5 min ago").font(.system(size: 10))
+                            }
+                            VStack {
+                                Circle().frame(width: 75, height: 75)
+                                Text("5 min ago").font(.system(size: 10))
+                                Circle().frame(width: 75, height: 75)
+                                Text("5 min ago").font(.system(size: 10))
+                            }
+                        }
+                    }
+                }.scrollTargetLayout()
+            }.padding(10).background(Color(red: 0.9, green: 0.9, blue: 0.9)).clipShape(RoundedRectangle(cornerRadius: 15)).overlay(RoundedRectangle(cornerRadius: 10).inset(by: 1).stroke(.red, lineWidth: 1)).frame(width: 350, height: 210).scrollTargetBehavior(.viewAligned)
+        }
     }
 }
 
 #Preview {
-    GroupCard()
+    GroupCard(groupName: "AA Workout Group")
 }
--- acountability101/views/HomeView.swift
+++ acountability101/views/HomeView.swift
@@ -9,7 +9,17 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Accountability101").foregroundStyle(Color.red).bold().font(.system(size: 32))
+            Button("Create or join a group") {
+                
+            }.foregroundStyle(.black).bold().padding(.horizontal, 40).padding(.vertical, 15).background(.red).clipShape(RoundedRectangle(cornerRadius: 15)).padding(.vertical, 25)
+            ScrollView {
+                GroupCard(groupName: "AA Workout Group")
+                GroupCard(groupName: "Reading Group")
+            }
+            Spacer()
+        }.padding(.horizontal, 20)
     }
 }
 
