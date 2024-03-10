e97902e0dcb2c2f396b16ed3e011481ead89dfe4
Connor McEnery <61913573+cjmcenery@users.noreply.github.com>
2024-03-10 09:54:26 -0500

    Initial Commit

---
+++ acountability101/posts/Post.swift
@@ -0,0 +1,8 @@
+//
+//  Post.swift
+//  acountability101
+//
+//  Created by Connor McEnery on 3/9/24.
+//
+
+import Foundation
---
+++ acountability101/views/GroupCard.swift
@@ -0,0 +1,18 @@
+//
+//  GroupCard.swift
+//  acountability101
+//
+//  Created by Connor McEnery on 3/9/24.
+//
+
+import SwiftUI
+
+struct GroupCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GroupCard()
+}
